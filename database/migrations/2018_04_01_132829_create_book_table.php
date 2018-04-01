ad0b50001d46becbdbc85e2bf35bed79b3353afa
y.kondo <k.yuto.cre@gmail.com>
2018-04-01 22:30:59 +0900

    php artisan make:migration create_book_table --create=books

---
+++ database/migrations/2018_04_01_132829_create_book_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
