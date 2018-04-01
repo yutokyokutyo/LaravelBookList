94528adc88a85e967b73ca5aa14af451b31f2045
y.kondo <k.yuto.cre@gmail.com>
2018-04-01 22:36:14 +0900

    タイトルのスキーマを追加

--- database/migrations/2018_04_01_132829_create_book_table.php
+++ database/migrations/2018_04_01_132829_create_book_table.php
@@ -14,6 +14,7 @@ class CreateBookTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
             $table->timestamps();
         });
     }
